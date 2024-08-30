8d17fd79a3f54cc41c80846b541232843b79d1b8
Felipe Tsuyoshi Suzuki <ftsuyoshisuzuki@gmail.com>
2024-08-30 23:37:51 +0000

    Criação da tela base e forms de disciplina

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,8 +1,21 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, RadioField
 from wtforms.validators import DataRequired
 
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
     submit = SubmitField('Submit')
+
+
+class RegisterSubjectForm(FlaskForm):
+    subject_name = StringField('Cadastre a nova disciplina e o semestre associado', validators=[DataRequired()])
+    semester = RadioField('Semestre', choices=[
+        ('1', '1º semestre'),
+        ('2', '2º semestre'),
+        ('3', '3º semestre'),
+        ('4', '4º semestre'),
+        ('5', '5º semestre'),
+        ('6', '6º semestre')
+    ], validators=[DataRequired()])
+    submit = SubmitField('Cadastrar')
